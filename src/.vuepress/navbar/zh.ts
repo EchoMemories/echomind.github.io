7092d88626f05e6ec6f533d25442198120780f6e
0x101008 <shtnb@outlook.com>
2025-05-25 15:20:42 +0800

    clear

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,31 +1,31 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  "/zh/",
-  "/zh/portfolio",
-  "/zh/demo/",
+  // "/zh/",
+  // "/zh/portfolio",
+  // "/zh/demo/",
+  // {
+  //   text: "指南",
+  //   icon: "lightbulb",
+  //   prefix: "/zh/guide/",
+  //   children: [
+  //     {
+  //       text: "Bar",
+  //       icon: "lightbulb",
+  //       prefix: "bar/",
+  //       children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
+  //     },
+  //     {
+  //       text: "Foo",
+  //       icon: "lightbulb",
+  //       prefix: "foo/",
+  //       children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
+  //     },
+  //   ],
+  // },
   {
-    text: "指南",
-    icon: "lightbulb",
-    prefix: "/zh/guide/",
-    children: [
-      {
-        text: "Bar",
-        icon: "lightbulb",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "ellipsis", link: "" }],
-      },
-      {
-        text: "Foo",
-        icon: "lightbulb",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "ellipsis", link: "" }],
-      },
-    ],
-  },
-  {
-    text: "V2 文档",
+    text: " 文档",
     icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    link: "/zh/",
   },
 ]);
--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -2,25 +2,25 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
   "/": [
-    "",
-    "portfolio",
-    {
-      text: "Demo",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
+    // "",
+    // "portfolio",
+    // {
+    //   text: "Demo",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
     {
       text: "Docs",
       icon: "book",
       prefix: "guide/",
       children: "structure",
     },
-    {
-      text: "Slides",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/plugins/markdown/revealjs/demo.html",
-    },
+    // {
+    //   text: "Slides",
+    //   icon: "person-chalkboard",
+    //   link: "https://ecosystem.vuejs.press/plugins/markdown/revealjs/demo.html",
+    // },
   ],
 });
